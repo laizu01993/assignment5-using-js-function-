06ab5ea1ff6db234aab67ebe9cf052f11671a26c
laizu01993 <laizuaktar6@gmail.com>
2024-09-23 16:03:23 +0600

    added add money to donation balance

--- js/donation.js
+++ js/donation.js
@@ -1,1 +1,19 @@
-console.log('added donation.js')
+// add money to the donation stat
+
+document.getElementById('btn-donate')
+.addEventListener('click', function(){
+    
+    // get money to be added to the donation account
+    const addMoneyInput = document.getElementById('input-donate-amount').value;
+    const addMoneyNumber = parseFloat(addMoneyInput);
+   
+    // get the donation balance
+    const donateBalance = document.getElementById('donation-balance').innerText;
+    const donateBalanceNumber = parseFloat(donateBalance);
+    
+    // add addMoneyInput with donation balance
+    const newBalance = addMoneyNumber + donateBalanceNumber;
+    console.log(newBalance);
+    // update the donation balance
+    document.getElementById('donation-balance').innerText = newBalance;
+})
