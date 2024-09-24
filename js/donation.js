ac759d68315600f96cb6af4dc4db5517de31458e
laizu01993 <laizuaktar6@gmail.com>
2024-09-25 04:19:38 +0600

    added validation and set history time

--- js/donation.js
+++ js/donation.js
@@ -6,42 +6,77 @@ document.getElementById('btn-donate')
     
     // get money to be added to the donation account
     const addMoneyInput = document.getElementById('input-donate-amount').value;
+    
     const addMoneyNumber = parseFloat(addMoneyInput);
-   
+    
+  //  validate input
+  if(isNaN(addMoneyNumber) || addMoneyNumber <= 0){
+    alert('Please enter a valid donation amount');
+    return;
+  }
+
     // get the donation balance
     const donateBalance = document.getElementById('donation-balance').innerText;
+    
     const donateBalanceNumber = parseFloat(donateBalance);
+   
     
-    // add addMoneyInput with donation balance
-    const newBalance = addMoneyNumber + donateBalanceNumber;
-    
-
-    // update the donation balance
-    document.getElementById('donation-balance').innerText = newBalance;
-
-    // if(addMoneyInput <= 0 || isNaN(addMoneyInput) || mainAccountBalance < addMoneyInput){
-    //     alert('Invalid Input');
-    //     return;
-    //  }
 
    // cash out from main balance
- const mainAccountBalance = document.getElementById('main-balance').innerText;
- const mainBalanceNumber = parseFloat(mainAccountBalance);
- const newMainBalance = mainBalanceNumber - addMoneyNumber;
- document.getElementById('main-balance').innerText = newMainBalance;
+  const mainAccountBalance = document.getElementById('main-balance').innerText;
+  const mainBalanceNumber = parseFloat(mainAccountBalance);
  
+  // validation
+  if(mainBalanceNumber < addMoneyNumber){
+    alert('You do not have enough money');
+    
+  
+
+  // set new main balance 
+  document.getElementById('donation-balance').innerText = donateBalanceNumber;
+  return;
+}
+
+  // the new donation balance
+  const newBalance = addMoneyNumber + donateBalanceNumber;
+
+   // update the donation balance
+   document.getElementById('donation-balance').innerText = newBalance;
+
+  // the new main balance
+  const newMainBalance = mainBalanceNumber - addMoneyNumber;
+ 
+
+ 
+//  update the main balance
+
+document.getElementById('main-balance').innerText = newMainBalance; 
+
+
 //  history section
+const dateTimeOptions = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  hour: '2-digit',
+  minute: '2-digit',
+  second:'2-digit',
+  hour12: false
+};
+
+const dateTimeString = new Date().toLocaleString('en-BD', dateTimeOptions);
+
 const div = document.createElement('div');
 div.classList.add('bg-[#FFFFFF]');
 div.innerHTML = `
   <h2 class="text-lg text-[#111111] font-bold"> ${addMoneyInput} Taka is Donated for famine-2024 at Noakhali,Bangladesh </h2>
-  <p> ${new Date().toLocaleDateString()}</p>
+  <p> Date: ${dateTimeString}</p>
               
-
 `
- document.getElementById('history-container').appendChild(div);
+document.getElementById('history-container').appendChild(div);
 
-    
+
 
    
     
