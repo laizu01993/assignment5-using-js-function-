ae2ae2ba6ccab143803524469ae64c2fac85ee65
laizu01993 <laizuaktar6@gmail.com>
2024-09-25 05:34:39 +0600

    solved the file path problem

--- js/blog.js
+++ js/blog.js
@@ -1,5 +1,5 @@
 document.getElementById('btn-blog').addEventListener('click', function(){
-    window.location.href = './blog.html'
+    window.location.href = '/blog.html'
 })
 document.getElementById('btn-home').addEventListener('click', function(){
     window.location.href = './index.html'
