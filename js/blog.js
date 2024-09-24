931fd900e0cc03102c7cf8a1531fc849223171ad
laizu01993 <laizuaktar6@gmail.com>
2024-09-25 05:31:36 +0600

    solved the file path

--- js/blog.js
+++ js/blog.js
@@ -1,7 +1,7 @@
 document.getElementById('btn-blog').addEventListener('click', function(){
-    window.location.href = '/blog.html'
+    window.location.href = './blog.html'
 })
 document.getElementById('btn-home').addEventListener('click', function(){
-    window.location.href = '/index.html'
+    window.location.href = './index.html'
 })
 
